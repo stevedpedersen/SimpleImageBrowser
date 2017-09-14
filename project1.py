71cb398e3b9e4110a77771146765a3252abfad63
Steve Pedersen <sp@steves-mbp.gateway.sonic.net>
2017-09-13 19:25:42 -0700

    finished all key browsing events and commented methods.

--- project1.py
+++ project1.py
@@ -42,7 +42,6 @@ class ImageBrowser(QWidget):
 		self.images = []
 		self.labels = [ClickableLabel(self),ClickableLabel(self),ClickableLabel(self),
 			ClickableLabel(self), ClickableLabel(self),ClickableLabel(self)]
-
 		self.initUI()		
 
 	def initUI(self):
@@ -50,10 +49,11 @@ class ImageBrowser(QWidget):
 		self.setWindowTitle(self.title)
 		self.setGeometry(0, 0, self.width, self.height)
 		self.initImages(self.files)
-		self.draw(0, 6) # mode & image
-		print(self.h,self.i,self.j)
+		# Start off in Thumbnail Mode on the first image
+		self.draw(0, 0) 
 		self.show()
 
+	# Populates a 2D List of Pixmaps with Thumbnail & Full versions of all images
 	def initImages(self, files):
 		thumbs = []
 		fulls = []
@@ -65,12 +65,10 @@ class ImageBrowser(QWidget):
 
 		self.images.append(thumbs)
 		self.images.append(fulls)
-		# self.images[0] = cycle(self.images[0])
-		# self.images[1] = cycle(self.images[1])
 
+	# Scale image to width or height based on image orientation	& label dimensions
 	def resizeAndFrame(self, filename, w, h, b):		
-		pixmap = QPixmap(filename)
-		# scale image to width or height based on image orientation	
+		pixmap = QPixmap(filename)		
 		if pixmap.width() > pixmap.height():
 			pixmap = pixmap.scaledToWidth(w - 2*b)
 			if pixmap.height() > (h - 2*b):
@@ -80,16 +78,19 @@ class ImageBrowser(QWidget):
 
 		return pixmap
 
+	# Attach images to labels in thumbnail or fullscreen mode
 	def draw(self, mode, LIndex, selected = -1):
 		if selected == -1:
 			selected = LIndex
 		self.h = (selected - 1) % len(self.files)
 		self.i = (selected) 	% len(self.files)
 		self.j = (selected + 1) % len(self.files)
-		self.mode = mode				
+		self.mode = mode
+
+		# Thumbnail Mode
 		if mode == 0:
 			self.clearBrowser()
-			y = (self.height - self.thumbH*5) / 2
+			y = self.height - self.thumbH * 2 
 			for i in range(5):	
 				thumb = selected+i if (selected+i < len(self.files)) else abs(len(self.files) - selected-i)
 				color = 'green'
@@ -101,8 +102,8 @@ class ImageBrowser(QWidget):
 				self.labels[i].setStyleSheet('border: ' + str(self.thumbB) + 'px solid '+ color)
 				self.labels[i].setObjectName('Label: {},\tMode: {}'.format(thumb, mode))
 				self.labels[i].clicked.connect(self.mouseSel)
-
-				
+		
+		# Full Screen Mode		
 		elif mode == 1:
 			self.clearBrowser()
 			y = (self.height - self.fullH) / 2
@@ -113,6 +114,7 @@ class ImageBrowser(QWidget):
 			self.labels[5].setObjectName('Label: {},\tMode: {}'.format(selected, mode))
 			self.labels[5].clicked.connect(self.mouseSel)
 	
+	# Handles key events and responds according to current browser state
 	def keyPressEvent(self, event):
 		up = 16777235
 		down = 16777237
@@ -126,6 +128,10 @@ class ImageBrowser(QWidget):
 			self.draw(1, self.h-1, self.h)
 		elif (self.mode == 1) and event.key() == right:
 			self.draw(1, self.h-1, self.j)
+		elif (self.mode == 0) and event.key() == left:
+			self.draw(0, self.h, self.h)
+		elif (self.mode == 0) and event.key() == right:
+			self.draw(0, self.h, self.j)
 
 	def mouseClickEvent(self, event):
 		print(event)
@@ -133,27 +139,15 @@ class ImageBrowser(QWidget):
 	def mouseSel(self, name):
 		print('"%s" clicked' % name)
 
+	# Hide any visible contents on browser window
 	def clearBrowser(self):
 		for i in range(6):
 			self.labels[i].setStyleSheet('border: none')
 			self.labels[i].clear()
 
+# Create an image browser from the images in the 'data' folder
 if __name__ == '__main__':
-	files = os.listdir('data')
 	app = QApplication(sys.argv)
-	imageBrowser = ImageBrowser(files)
+	imageBrowser = ImageBrowser(os.listdir('data'))
 	sys.exit(app.exec_())    
 	
-# NOTES: Circular list operations
-# i = len(l) - 1
-# jIndex = (i - 1) % len(l)
-# kIndex = (i + 1) % len(l)
-
-# j = l[jIndex]
-# k = l[kIndex]
-
-# Or, to be less verbose:
-
-# k = l[(i + 1) % len(l)]
-
-# note that if 0 <= i < len(l), then l[(i + 1) % len(l)] can also be written l[i - (len(l)-1)], avoiding the modulo.
